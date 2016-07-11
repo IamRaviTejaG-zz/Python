16cfb2bea59895826321fd2594b4fd9760c091d9
Ravi Teja Gannavarapu <teja.gannavarapu@gmail.com>
2016-07-12 00:02:56 +0530

    Add files via upload

---
+++ Area Of Square & Rectangle.py
@@ -0,0 +1,15 @@
+# Program to calculate the area of a square or rectangle.
+# Created By G.Ravi Teja in 2015.
+
+def area (leng, brd):
+        ar = leng*brd;
+        if (leng==brd):
+                print ("\nArea of the square is " + str(ar) + " square units.");
+        else:
+                print ("\nArea of the rectangle is " + str(ar) + " square units.");
+
+leng = input("Enter the length: ");
+brd = input("\nEnter the breadth: ");
+area (leng, brd);
+
+x = raw_input("\nPress any key to exit.");
