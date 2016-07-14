9e5339f0c527af40480d7631e137f76fe3db28c8
Ravi Teja Gannavarapu <teja.gannavarapu@gmail.com>
2016-07-14 16:53:53 +0530

    Add files via upload

---
+++ Int to Bin Oct Hex.py
@@ -0,0 +1,7 @@
+a = raw_input("Enter a number to view its Binary, Octal & Hexadecimal representation: ");
+b = int(a);
+print ("\n\n");
+print (str(b) + " in Binary: " + str(bin(b)));
+print (str(b) + " in Octal: " + str(oct(b)));
+print (str(b) + " in Hexadecimal: " + str(hex(b)));
+x = raw_input("\n\nPress any key exit.");
