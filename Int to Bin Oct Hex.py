dc63df75044f306acd8b6c7037dd9c38df005d40
Ravi Teja Gannavarapu <teja.gannavarapu@gmail.com>
2016-08-02 23:56:51 +0530

    Delete Int to Bin Oct Hex.py

--- Int to Bin Oct Hex.py
+++
@@ -1,7 +0,0 @@
-a = raw_input("Enter a number to view its Binary, Octal & Hexadecimal representation: ");
-b = int(a);
-print ("\n\n");
-print (str(b) + " in Binary: " + bin(b));
-print (str(b) + " in Octal: " + oct(b));
-print (str(b) + " in Hexadecimal: " + hex(b));
-x = raw_input("\n\nPress any key exit.");
