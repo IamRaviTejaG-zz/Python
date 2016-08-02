0cf632717cd504a7d1f15b3bcfe4ff48b9ef2bf6
Ravi Teja Gannavarapu <teja.gannavarapu@gmail.com>
2016-08-02 23:57:40 +0530

    Add files via upload

---
+++ Even & Odd.py
@@ -0,0 +1,20 @@
+# Created By G.Ravi Teja in November 2015.
+# Updated in May, June 2016.
+""" Updated the code with str() and abs() and the use of function. """
+
+def oddeven (num):
+   snum = str(num)
+
+   if (num<0):
+      num = abs(num)
+
+   if (num%2==0):
+      print ("\n" + snum + " is even.")
+
+   else:
+      print ("\n" + snum + " is odd.")
+
+num = input("Enter a number: ")
+oddeven (num)
+
+x = raw_input("\nPress any key to exit.")
---
+++ Int to Bin Oct Hex.py
@@ -0,0 +1,7 @@
+a = raw_input("Enter a number to view its Binary, Octal & Hexadecimal representation: ")
+b = int(a)
+print ("\n\n")
+print (str(b) + " in Binary: " + bin(b))
+print (str(b) + " in Octal: " + oct(b))
+print (str(b) + " in Hexadecimal: " + hex(b))
+x = raw_input("\n\nPress any key exit.")
