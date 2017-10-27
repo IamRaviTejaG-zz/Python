e5a0531ec7b79f2ba3433776e6653996b5a9ae77
Ravi Teja Gannavarapu <IamRaviTejaG@users.noreply.github.com>
2017-10-28 01:37:01 +0530

    Update Even & Odd.py

--- Even & Odd.py
+++ Even & Odd.py
@@ -1,16 +1,9 @@
-# Created By G.Ravi Teja in November 2015.
-# Updated in May, June 2016.
-""" Updated the code with str() and abs() and the use of function. """
-
 def oddeven (num):
    snum = str(num)
-
    if (num<0):
       num = abs(num)
-
    if (num%2==0):
       print ("\n" + snum + " is even.")
-
    else:
       print ("\n" + snum + " is odd.")
 
